39d4734ff07f44db7d95ee9317f6a803e53aadcb
ryuta-mitsunaga <mag03w@yahoo.co.jp>
2023-12-09 14:06:41 +0900

    seederの変更

--- database/seeders/StudentTableSeeder.php
+++ database/seeders/StudentTableSeeder.php
@@ -19,12 +19,31 @@ class StudentTableSeeder extends Seeder
         DB::table('student')->insert([
             'name_sei' => '山田',
             'name_mei' => '太郎',
-            'email' => 'tes@a.jp',
+            'email' => 'test1@a.jp',
             'password' => bcrypt('11111111'),
             'remember_token' => '',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
+        DB::table('student')->insert([
+            'name_sei' => '鈴木',
+            'name_mei' => '太郎',
+            'email' => 'test2@a.jp',
+            'password' => bcrypt('11111111'),
+            'remember_token' => '',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('student')->insert([
+            'name_sei' => '藤井',
+            'name_mei' => '太郎',
+            'email' => 'test3@a.jp',
+            'password' => bcrypt('11111111'),
+            'remember_token' => '',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
